b94af80ae83a76bf6d9009da25c2dcdc10bd0b81
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-10-19 22:22:27 +0300

    Initial Commit

---
+++ JSON Placeholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON Placeholder
+//
+//  Created by Pavel Semenchenko on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
